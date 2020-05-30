cfb472a95e84108f1f4e53fd7ddfcd7fc1b87657
Katariamo <57604126+Katariamo@users.noreply.github.com>
2020-05-31 01:20:24 +0300

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a = 1;
+        int b = 2;
+         int c = a+b;
+        System.out.println(c);
+
+
+
+    }
+}
